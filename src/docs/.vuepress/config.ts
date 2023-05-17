72f8872603521ac936f8b0ea7fcdb65411439a09
nibazshab <936100289@qq.com>
2023-05-17 22:58:30 +0800

    themePlugins

--- src/docs/.vuepress/config.ts
+++ src/docs/.vuepress/config.ts
@@ -1,4 +1,4 @@
-import { defineUserConfig, defaultTheme } from 'vuepress';
+import { defineUserConfig, defaultTheme, themePlugins } from 'vuepress';
 import { getDirname, path } from '@vuepress/utils';
 import { shikiPlugin } from '@vuepress/plugin-shiki';
 import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
@@ -48,6 +48,7 @@ export default defineUserConfig({
   alias: {
     '@theme/PageNav.vue': path.resolve(__dirname, './components/PageNav.vue'),
   },
+  themePlugins.externalLinkIcon: false,
   plugins: [
     shikiPlugin({
       theme: 'github-dark',
